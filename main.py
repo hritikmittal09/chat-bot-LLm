a2924dfe78b2f9e4d7de0c40dc67e836cfd243b5
Your Name <@example.com>
2025-09-11 21:46:12 +0530

    configure runing commands in main .py file

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 from langchain_ollama import OllamaLLM
 from spech import speak
+import os
 
+
+def cmdbotTest():
 # ✅ Initialize Ollama with your model
-llm = OllamaLLM(model="zera")   # replace with your model name
+    llm = OllamaLLM(model="zera")   # replace with your model name
 
-print("🤖 Chatbot Ready! Type 'quit' or 'exit' to stop.\n")
+    print("🤖 Chatbot Ready! Type 'quit' or 'exit' to stop.\n")
 
-userInput = ""  # initialize variable
+    userInput = ""  # initialize variable
 
-while userInput.lower() not in ["quit", "exit"]:
-    userInput = input("You: ")
-    if userInput.lower() in ["quit", "exit"]:
-        print("👋 Goodbye!")
-        break
+    while userInput.lower() not in ["quit", "exit"]:
+        userInput = input("You: ")
+        if userInput.lower() in ["quit", "exit"]:
+            print("👋 Goodbye!")
+            break
 
-    # ✅ Ask a question
-    response = llm.invoke(userInput)
-    print("thinking ...")
+        # ✅ Ask a question
+        response = llm.invoke(userInput)
+        print("thinking ...")
 
-    print("Bot:", response)
-    speak(response)
+        print("Bot:", response)
+        speak(response)
+
+def Run():
+    
+
+# Replace "cmd_command" with the actual command you want to run
+    cmd_command = "streamlit run Gui.py"
+
+# Execute the command using os.system()
+    os.system(cmd_command)
+if __name__ == "__main__":
+    Run()
+
