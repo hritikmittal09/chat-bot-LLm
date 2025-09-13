99e1b4faf72a17da528574e1afd2fba34aebd9a8
Your Name <@example.com>
2025-09-13 21:27:32 +0530

    load file path fuction ade

--- File_Explore.py
+++ File_Explore.py
@@ -1,13 +1,30 @@
 import json
+import webbrowser
 import os 
-files = { "Spotify" : r"C:\Users\Lenovo\AppData\Roaming\Spotify\Spotify.exe" }
+files = dict()
+
+
+
+def loadFilesPaths():
+   try:
+     with open("filepaths.json") as f :
+      global files
+      files = json.load(f)
+      return
+   except Exception as e :
+      print("Unble to load file paths")
  
 def getShortcutsList():
+ loadFilesPaths()
  return list(files.keys())
-
-def openFile(fileName):
- if fileName == '':
-  return
- else :
-  os.system(files[fileName])
-  return
+def openFile(fileName :str):
+    if fileName == '':
+        return
+    elif files[fileName].startswith('http') :
+       webbrowser.open(files[fileName])
+       print("i am here !")
+       return
+    else :
+        
+        os.startfile( files[fileName])
+        return
