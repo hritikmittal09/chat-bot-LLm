6cba16f9257f35bd3214a9734bee46f3998b516e
Your Name <@example.com>
2026-03-19 12:27:41 +0530

    build a list for user modal

--- coding-agent/Client.py
+++ coding-agent/Client.py
@@ -1,16 +1,21 @@
 from google import genai
 from dotenv import load_dotenv
+from google.genai import types
 import os
 
 load_dotenv()
 
 GAMINI_API_KEY = os.getenv("GAMNI_API-KEY")
 prompt = input("enter prompt")
+messages =[
+     types.Content(role="user", parts=[types.Part(text=prompt)])
+]
 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
 client = genai.Client(api_key=GAMINI_API_KEY)
 
 response = client.models.generate_content(
-model="gemini-2.5-flash-lite", contents= f"{prompt}7"
+model="gemini-2.5-flash-lite",
+ contents= messages
 )
 print(response.text)
