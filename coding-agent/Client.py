194a4254ed5da1365465378a0ae9018ccf0678a4
Your Name <@example.com>
2026-03-20 23:35:41 +0530

    get vontrnt

--- coding-agent/Client.py
+++ coding-agent/Client.py
@@ -2,13 +2,15 @@ from google import genai
 from dotenv import load_dotenv
 from google.genai import types
 from functions.getFileInfo import get_files_info
+from functions.get_file_content import get_file_content
+
 import os
 
 load_dotenv()
 
 GAMINI_API_KEY = os.getenv("GAMNI_API-KEY")
 
-print(get_files_info("Calculartor/pkg"))
+print(get_file_content("Calculartor", "icalcular.py"))
 prompt = input("enter prompt")
 messages =[
      types.Content(role="user", parts=[types.Part(text=prompt)])
