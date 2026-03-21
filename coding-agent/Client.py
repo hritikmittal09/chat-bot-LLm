5c3919a25b2577a9071d3de21d46b04c8bd50696
Your Name <@example.com>
2026-03-21 15:44:43 +0530

    tools added

--- coding-agent/Client.py
+++ coding-agent/Client.py
@@ -2,6 +2,9 @@ from google import genai
 from dotenv import load_dotenv
 from google.genai import types
 from functions.getFileInfo import get_files_info, schema_get_files_info
+from functions.get_file_content import schema_get_file_content
+from functions.run_python_file import schema_run_python_file
+from functions.write_file import schema_make_dir, schema_write_file
 
 import os
 
@@ -17,8 +20,26 @@ tool_map = {
 
 
 config = types.GenerateContentConfig(
-    system_instruction="You are a helpful coding agent. Use the get_files_info tool to list files when asked.",
-    tools=[types.Tool(function_declarations=[schema_get_files_info])],  # ✅ wrap it
+    system_instruction=(
+        "You are a helpful coding agent.\n"
+        "Use get_files_info to list files when needed.\n"
+        "Use get_file_content to read file contents when the user asks to open/read a file.\n"
+        "use run_python_file to run a specific python file when needed when user asks run .\n"
+        "use make_dir to make a directory when needed\n" 
+        "use write_file to write or overwrite the files if needed\n"
+        "Always call the appropriate tool instead of guessing file content.\n"
+    ),
+    tools=[
+        types.Tool(
+            function_declarations=[
+                schema_get_files_info,
+                schema_get_file_content,  # ✅ fixed
+                schema_run_python_file,
+                schema_make_dir,
+                schema_write_file
+            ]
+        )
+    ],
 )
 
 messages = [
