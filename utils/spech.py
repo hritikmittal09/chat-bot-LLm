da1efc8ddf0298b8dc89b1a2e659320dfb7222a4
hritik <hritikmittal77@gmal.com>
2026-05-31 21:10:06 +0530

    pick only feama xoice for zera

--- utils/spech.py
+++ utils/spech.py
@@ -10,6 +10,21 @@ def speak(text):
     # For Mac, If you face error related to "pyobjc" when running the `init()` method :
     # Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
 
+    # Set to female voice
+    voices = engine.getProperty('voices')
+    # Select female voice (usually at index 1, but search for female voice)
+    female_voice = None
+    for voice in voices:
+        if 'female' in voice.name.lower():
+            female_voice = voice.id
+            break
+    # If no female voice found with 'female' in name, use second voice (usually female)
+    if female_voice is None and len(voices) > 1:
+        female_voice = voices[1].id
+    
+    if female_voice:
+        engine.setProperty('voice', female_voice)
+    
     engine.say(f"{text}")
     engine.runAndWait()
     
