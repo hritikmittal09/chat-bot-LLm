92959de26faeefe9118f0532bfb1e1d23aaf08cf
hritik <hritikmittal77@gmal.com>
2026-06-20 14:09:41 +0530

    spech recongnation improvements

--- utils/voice_input.py
+++ utils/voice_input.py
@@ -3,22 +3,45 @@ import speech_recognition as sr
 def user_voiceInput():
     recognizer = sr.Recognizer()
 
-    with sr.Microphone() as source:
-        print("🎤 Say something...")
-        recognizer.adjust_for_ambient_noise(source)
-        audio = recognizer.listen(source)
+    # Threshold settings
+    recognizer.pause_threshold = 1.0       
+    recognizer.energy_threshold = 300     
+    recognizer.dynamic_energy_threshold = True
 
     try:
-        text = recognizer.recognize_google(audio)
-        print("✅ You said:", text)
-        return text
+        with sr.Microphone() as source:
+            print("🎤 Say something...")
 
-    except sr.UnknownValueError:
-        print("❌ Could not understand audio.")
+            
+            recognizer.adjust_for_ambient_noise(source, duration=1)
+
+            audio = recognizer.listen(
+                source,
+                timeout=2,              
+            )
+
+        try:
+            text = recognizer.recognize_google(audio)
+
+            print("✅ You said:", text)
+            return text
+
+        except sr.UnknownValueError:
+            print("❌ Could not understand audio.")
+            return None
+
+        except sr.RequestError as e:
+            print(f"⚠️ API unavailable: {e}")
+            return None
+
+    except sr.WaitTimeoutError:
+        print("⌛ No speech detected.")
         return None
-    except sr.RequestError:
-        print("⚠️ API unavailable or internet issue.")
+
+    except Exception as e:
+        print(f"⚠️ Error: {e}")
         return None
+
 
 if __name__ == "__main__":
     user_voiceInput()
